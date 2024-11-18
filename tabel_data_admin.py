3cc2f5a0525ecf89e7995f673dd8c6fec89a1e09
ade781 <110089945+ade781@users.noreply.github.com>
2024-11-18 21:41:15 +0700

    Update tabel_data_admin.py

--- tabel_data_admin.py
+++ tabel_data_admin.py
@@ -7,7 +7,6 @@ from db_connection import connect_db
 def main():
     st.title("Data KOMANDO")
 
-    # Add custom CSS for table styling
     st.markdown("""
         <style>
         .dataframe {
@@ -33,12 +32,10 @@ def main():
         </style>
     """, unsafe_allow_html=True)
 
-    # Cek login
     if 'is_authenticated' not in st.session_state or not st.session_state.is_authenticated:
         st.error("Silakan login terlebih dahulu!")
         return
 
-    # Ambil data dari database untuk tabel
     conn = connect_db()
     cursor = conn.cursor()
     query = """
@@ -53,31 +50,26 @@ def main():
         st.info("Tidak ada data pengguna untuk ditampilkan.")
         return
 
-    # Tambahkan nomor secara otomatis untuk setiap baris data
     table_data = [{"Nomor": i + 1, "ID": row[0], "Username": row[1], "Password": row[2]}
                   for i, row in enumerate(data)]
 
     df = pd.DataFrame(table_data)
 
-    # Define custom column widths
     col_widths = {
         'Nomor': '10%',
         'ID': '10%',
         'Username': '25%',
-        'Password': '55%'  # Kolom Password menggunakan lebih banyak lebar
+        'Password': '55%' 
     }
 
-    # Create the HTML table with custom column widths
     html_table = df.to_html(index=False, classes='dataframe')
 
-    # Add column width styles
     style_header = "<style>\n"
     for col, width in col_widths.items():
         style_header += f".dataframe td:nth-child({list(col_widths.keys()).index(col) + 1}) {{ width: {width}; }}\n"
         style_header += f".dataframe th:nth-child({list(col_widths.keys()).index(col) + 1}) {{ width: {width}; }}\n"
     style_header += "</style>"
 
-    # Display the table with custom styling
     st.markdown(style_header + html_table, unsafe_allow_html=True)
 
 
