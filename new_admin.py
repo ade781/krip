4223489ae0e4ab381ecd6e84f8ec5e94262fef82
ade781 <110089945+ade781@users.noreply.github.com>
2024-11-18 21:39:03 +0700

    Update new_admin.py

--- new_admin.py
+++ new_admin.py
@@ -6,7 +6,6 @@ from db_connection import connect_db
 def main():
     st.title("Daftar Komandan Baru (Admin)")
 
-    # Deskripsi tentang pentingnya pemilihan admin
     st.markdown("""
     ### Penting!
     **Hanya pengguna yang benar-benar terpercaya yang seharusnya memiliki akses sebagai admin.**
@@ -14,28 +13,22 @@ def main():
     Harap pastikan bahwa yang didaftarkan memiliki hak dan wewenang yang sesuai.
     """)
 
-    # Input untuk username, password, konfirmasi password, dan role
     username = st.text_input("Username", key="new_username_input")
     password = st.text_input(
         "Password", type="password", key="new_password_input")
     password_confirm = st.text_input(
         "Konfirmasi Password", type="password", key="confirm_password_input")
 
-    # Role selalu admin pada pendaftaran ini
-    role = "admin"  # Karena ini hanya untuk admin, role default adalah admin
+    role = "admin"  
 
-    # Tombol untuk proses pendaftaran
     if st.button("Daftar", key="submit_register"):
         if password == password_confirm:
             try:
-                # Hash password dengan bcrypt
                 salt = bcrypt.gensalt()
                 hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
-                # Ubah hash menjadi string
                 hashed_password_str = hashed_password.decode('utf-8')
 
-                # Simpan ke database dengan role yang dipilih
                 conn = connect_db()
                 cursor = conn.cursor()
                 cursor.execute(
@@ -54,10 +47,8 @@ def main():
         else:
             st.error("Password dan konfirmasi password tidak cocok.")
 
-    # Tombol untuk kembali ke halaman login
     if st.button("Kembali ke Halaman Login", key="back_to_login"):
         st.session_state.is_registering = False
         st.session_state.page = "login"
 
-    # Peringatan untuk pengguna
     st.warning("**Peringatan:** Pengguna yang ditambahkan sebagai admin harus benar-benar dapat dipercaya, karena memiliki akses penuh ke sistem ini. Pastikan Anda memilih dengan bijak!")
