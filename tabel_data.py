f5fdd2f119fc34f5f58a455308b859523fd184fc
ade781 <110089945+ade781@users.noreply.github.com>
2024-11-18 21:40:51 +0700

    Update tabel_data.py

--- tabel_data.py
+++ tabel_data.py
@@ -7,7 +7,6 @@ from db_connection import connect_db
 def main():
     st.title("Data Tabel Enkripsi")
 
-    # Add custom CSS for table styling
     st.markdown("""
         <style>
         .dataframe {
@@ -33,19 +32,15 @@ def main():
         </style>
     """, unsafe_allow_html=True)
 
-    # Cek login
     if 'is_authenticated' not in st.session_state or not st.session_state.is_authenticated:
         st.error("Silakan login terlebih dahulu!")
         return
 
-    # Fitur pencarian berdasarkan username
     username_search = st.text_input("Cari berdasarkan Username:", "")
 
-    # Cek ID user login
     if 'user_id' in st.session_state:
         user_id = st.session_state.user_id
 
-        # Ambil data dari database berdasarkan username jika ada input pencarian
         conn = connect_db()
         cursor = conn.cursor()
 
@@ -93,13 +88,11 @@ def main():
             st.info("Tidak ada data untuk ditampilkan.")
             return
 
-        # Tambahkan nomor secara otomatis untuk setiap baris data
         table_data = [{"Nomor": i + 1, "ID": row[0], "ID_User": row[1], "Username": row[2], "Isi": row[3], "Key": row[4], "Nama Operasi": row[5]}
                       for i, row in enumerate(data)]
 
         df = pd.DataFrame(table_data)
 
-        # Define custom column widths
         col_widths = {
             'Nomor': '7%',
             'ID': '7%',
@@ -107,20 +100,17 @@ def main():
             'Username': '15%',
             'Isi': '45%',
             'Key': '10%',
-            'Nama Operasi': '15%'  # Kolom Nama Operasi
+            'Nama Operasi': '15%' 
         }
 
-        # Create the HTML table with custom column widths
         html_table = df.to_html(index=False, classes='dataframe')
 
-        # Add column width styles
         style_header = "<style>\n"
         for col, width in col_widths.items():
             style_header += f".dataframe td:nth-child({list(col_widths.keys()).index(col) + 1}) {{ width: {width}; }}\n"
             style_header += f".dataframe th:nth-child({list(col_widths.keys()).index(col) + 1}) {{ width: {width}; }}\n"
         style_header += "</style>"
 
-        # Display the table with custom styling
         st.markdown(style_header + html_table, unsafe_allow_html=True)
 
 
