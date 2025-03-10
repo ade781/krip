e2f0d18f3e15456ea7adb435821f0a35609c2f73
ade781 <110089945+ade781@users.noreply.github.com>
2025-03-10 22:18:45 +0700

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -3,9 +3,9 @@ import mysql.connector
 
 def connect_db():
     return mysql.connector.connect(
-        host="6xdib.h.filess.io",
-        user="kripto_beingever",
-        password="a3badf670f3e69c9fe35d6e9688eeeccc6d0b8b5",
-        database="kripto_beingever",
-        port=3307
+        host="35.225.207.4",
+        user="root",
+        password="",
+        database="kripto",
+        port=3306
     )
